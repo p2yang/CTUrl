9c69370f4a60bee831f963b4baf0f12c95a35957
p2yang <p2yang@163.com>
2019-06-21 17:50:11 +0800

    feat: copy success

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Chrome 插件 - cturl
+# Chrome 插件 - CTUrl
 > 获取当前页面 title + url 到剪贴板, 默认格式为 `[title]\(url\)`
--- cturl.js
+++ cturl.js
@@ -1,19 +1,5 @@
-document.addEventListener('DOMContentLoaded', function()
-{
-    console.log('我被执行了！!!');
-    
-    const text = `[${document.title}]${location.href}`
+document.addEventListener('DOMContentLoaded', function() {
+    const text = `[${document.title}](${location.href})`
 
-    chrome.runtime.sendMessage({ message: text }, function(response) {
-        console.log('收到来自后台的回复：' + response);
-    });
+    chrome.runtime.sendMessage({ message: text });
 });
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
-{
-	// console.log(sender.tab ?"from a content script:" + sender.tab.url :"from the extension");
-	if(request.cmd == 'test') alert(request.value);
-	sendResponse('我收到了你的消息！');
-});
-
-
